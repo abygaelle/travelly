45511b7232972a98706985806332455c10e5a2d4
Abygaelle <abygaelleb02@gmail.com>
2022-04-12 15:45:34 -0400

    add travel post index

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,13 +1,15 @@
 # core/views.py 
 
 from flask import render_template, request, Blueprint
+from myapp.models import BlogPost
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    travel_posts = TravelPost.query.order_by(TravelPost.date.desc()).paginate(page=page, per_page=10)
+    return render_template('index.html', travel_posts=travel_posts)
 
 @core.route('/info')
 def info():
